9cdbf5e502c2d92abe00cb2f1c0ac865c16fb7eb
Willy <97862795+JongsuLee@users.noreply.github.com>
2022-12-04 15:57:19 +0900

    Update crawlingByFruitName_kurly.py

--- crawlingByFruitName_kurly.py
+++ crawlingByFruitName_kurly.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -14,7 +13,7 @@ def crawl(item):
     options.add_argument('--no-sandbox')
     options.add_argument('--single-process')
     options.add_argument('--disable-dev-shm-usage')
-    service = Service(ChromeDriverManager().install())
+    service = Service('chromedriver')
     driver = webdriver.Chrome(service=service, options=options)
 
     kurly = 'https://www.kurly.com/main'
